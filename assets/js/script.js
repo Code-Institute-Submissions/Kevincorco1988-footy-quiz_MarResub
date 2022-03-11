895dd6c55cae8327f9c9b73294ede5ca2cb51be3
Kevincorco1988 <kevincorco1988@gmail.com>
2022-03-11 04:35:05 +0000

    Add some css styling to the quiz, add question lists to js

---
+++ assets/js/script.js
@@ -0,0 +1,113 @@
+// Question lists
+
+var questionListOne = [
+    {
+        question: "Can you name the only club to win back-to-back League of Ireland Premier Division titles during the 90s?",
+    choice : ["St Patricks Athletic", "Cork City", "Shelbourne", "Derry City"],
+    answer: "St Patricks Athletic"
+    },
+    {
+        question: "For three separate seasons during the 90s, no player scored more goals in the Bundesliga than this man. What's his name?",
+    choice : ["Gerd Müller", "Fredi Bobic", "Ulf Kirsten", "Marco bode"],
+    answer: "Ulf Kirsten"
+    },
+    {
+        question: "Do you know who was named Best Young Player at the 1990 World Cup?",
+    choice : ["Andreas Moller", "Paolo Maldini", "Robert Prosinecki", "Paul Gascoigne"],
+    answer: "Robert Prosinecki"
+    },
+    {
+        question: "AC Milan broke the world transfer record in 1992 when they spent £13million on Gianluigi Lentini. From which club was he signed?",
+    choice : ["Lazio", "Torino", "Atlanta", "Sampdoria"],
+    answer: "Torino"
+    },
+    {
+        question: "Who scored the first Ireland goal of Mick McCarthy's reign as manager, which came in a friendly against Croatia in June 1996?",
+    choice : ["Keith O'Neill", "Tony Cascarino", "Niall Quinn", "Mark Kennedy"],
+    answer: "Keith O'Neill"
+    },
+]
+
+var questionListTwo = [
+    {
+        question: "Which player finished the 1999-2000 Premier League season as top scorer with 30 goals?",
+    choice : ["Alan Shearer", "Dwight Yorke", "Andy Cole", "Kevin Phillips"],
+    answer: "Kevin Phillips"
+    },
+    {
+        question: "Chelsea made five signings each costing £15m or more in the summer of 2003. Hernán Crespo, Juan Sebastián Verón, Claude Makélélé and Damien Duff were four of them. Who was the fifth?",
+    choice : ["Joe Cole", "Scott Parker", "Didier Drogba", "Adrian Mutu"],
+    answer: "Adrian Mutu"
+    },
+    {
+        question: "José Mourinho won the Champions League when his Porto side beat Monaco 3-0 in the 2004 final. Who was managing Monaco?",
+    choice : ["Claude Puel", "Didier Deschamps", "Francesco Guidolin", "Jean Tigana"],
+    answer: "Didier Deschamps"
+    },
+    {
+        question: "In 2006 Alan Shearer scored his final goal in professional football. Who was it against?",
+    choice : ["Liverpool", "Sunderland", "Chelsea", "Everton"],
+    answer: "Sunderland"
+    },
+    {
+        question: "Who scored the only goal of the game as Spain beat Germany 1-0 to win the European Championship in 2008?",
+    choice : ["Andrés Iniesta", "Xavi", "Fernando Torres", "David Villa"],
+    answer: "Fernando Torres"
+    },
+]
+
+var questionListThree = [
+    {
+        question: "Who was top of the Premier League on 1 January 2010?",
+    choice : ["Arsenal", "Chelsea", "Manchester United", "Manchester City"],
+    answer: "Chelsea"
+    },
+    {
+        question: "Who assisted Sergio Aguero's title-winning goal against QPR in May 2012?",
+    choice : ["Carlos Tevez", "James Milner", "Mario Balotelli", "Pablo Zabaleta"],
+    answer: "Mario Balotelli"
+    },
+    {
+        question: "Who was the most expensive player of the decade?",
+    choice : ["Neymar", "Philippe Coutinho", "Kylian Mbappe", "Antoine Griezmann"],
+    answer: "Neymar"
+    },
+    {
+        question: "Who provided the cross for Wayne Rooney’s bicycle kick against Manchester City in 2011?",
+    choice : ["Ryan Giggs", "Nani", "Paul Scholes", "Darren Fletcher"],
+    answer: "Nani"
+    },
+    {
+        question: "Who scored two penalties as Newcastle came back from 4-0 down to draw 4-4 with Arsenal in 2011?",
+    choice : ["Chieck Tiote", "Joey Barton", "Kevin Nolan", "Leon Best"],
+    answer: "Joey Barton"
+    },
+]
+
+var questionListFour = [
+    {
+        question: "Cristiano Ronaldo and which other player scored five goals at Euro 2020?",
+    choice : ["Karim Benzema", "Emil Forsberg", "Romelu Lukaku", "Patrik Schick"],
+    answer: "Patrik Schick"
+    },
+    {
+        question: "Frank Lampard was sacked by Chelsea in January 2021- but how long did his spell last?",
+    choice : ["12 months", "18 months", "24 months", "30 months"],
+    answer: "18 months"
+    },
+    {
+        question: "Which team were crowned La Liga champions in May 2021?",
+    choice : ["Atletico Madrid", "Barcelona", "Real Madrid", "Sevilla"],
+    answer: "Atletico Madrid"
+    },
+    {
+        question: "Who was Serie A top goalscorer with 29 goals in 2020-21?",
+    choice : ["Zlatan Ibrahimovic", "Cristiano Ronaldo", "Romelu Lukaku", "Luis Muriel"],
+    answer: "Cristiano Ronaldo"
+    },
+    {
+        question: "Robert Lewandowski scored how many goals in 2021?",
+    choice : ["69", "73", "79", "87"],
+    answer: ""
+    },
+]
